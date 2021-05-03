d9717a713510ae6c289d1be4c695a32f9872e159
ZtXavier <1491918111@qq.com>
2021-05-03 23:42:36 +0800

    zt

--- vfork.c
+++ vfork.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<sys/types.h>
 #include<unistd.h>
+#include<stdlib.h>
+
 
 int globvar = 5;
 
@@ -11,13 +13,32 @@ int main(void){
 
     printf("fork is different with vfork\n");
 
-    pid = fork();
-    /* pid = vfork(); */
+    /* pid = fork(); */
+    pid = vfork();
     switch(pid){
         case 0:
-        i=3;
+        i = 3;
         while(i-- > 0){
-            
+            printf("Child process is running\n");
+            globvar++;
+            var++;
+            sleep(1);
         }
+            printf("Child's globvar = %d,var = %d\n",globvar,var);
+        break;
+        case -1:
+            perror("Process creation failed\n");
+            exit(0);
+        default:
+        i = 5;
+        while(i-- >0){
+            printf("Parent process is running\n");
+            globvar++;
+            var++;
+            sleep(1);
+        }
+            printf("Parent's globvar = %d,var = %d\n",globvar,var);
+            
     }
+    exit(0);
 }
