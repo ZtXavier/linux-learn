cf220163685ff7a8604f5b564bc795138d5b78fc
ZtXavier <1491918111@qq.com>
2021-05-31 21:59:54 +0800

    zt

--- my_signal.c
+++ my_signal.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<signal.h>
+#include<unistd.h>
+#include<stdlib.h>
 
 /* 信号处理函数 */
 void handler_sigint(int signo){
@@ -9,7 +11,9 @@ void handler_sigint(int signo){
 int main(void){
     /* 安装信号处理函数 */
     signal(SIGINT,handler_sigint);
-    while(1)
-       ;
+    while(1){
+       sleep(3);
+       printf("正在工作\n");
+    }
     return 0;
 }
---
+++ test_setitimer.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<signal.h>
+#include<sys/time.h>
+#include<unistd.h>
+
+/* 信号处理程序 */
+void handler_sigtime(int signo){
+    switch (signo){
+        case SIGALRM:
+        printf("recv SIGALRM\n");
+        break;
+        case SIGPROF:
+        printf("recv SIGPROF\n");
+        break;
+        default:
+        break;
+    }
+}
+int main(){
+    struct itimerval value;
+
+    /* 安装信号处理函数 */
+    signal(SIGALRM,handler_sigtime);
+    signal(SIGPROF,handler_sigtime);
+    
+    /* 初始化value结构 */
+    value.it_value.tv_sec = 1;  //第一次1秒触发信号
+    value.it_value.tv_usec = 0; 
+    value.it_interval.tv_sec = 5;  //第二次开始每5秒触发信号
+    value.it_interval.tv_usec = 0;
+
+    /* 设置定时器(两种计时器) */
+    setitimer(ITIMER_REAL,&value,NULL); //当计时到达时会给进程发送SIGALRM的信号
+    setitimer(ITIMER_PROF,&value,NULL); //当计时到达时会给进程发送SIGPROF的信号
+    
+    while(1)
+
+        ;
+
+    return 0;
+}
